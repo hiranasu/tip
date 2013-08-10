d27da557662d4f339a6059f6bc86c996e1b8211e
mhirakaw <mhirakaw@yahoo-corp.jp>
2013-08-10 15:33:00 +0900

    tag_idは、タグから飛んでくる名前をそのまま使うようにDDL変更

--- tip/conf/evolutions/default/1.sql
+++ tip/conf/evolutions/default/1.sql
@@ -1,7 +1,7 @@
 # --- !Ups
 CREATE TABLE ROOM_INFO (
     room_id INT(10) NOT NULL,
-    tag_id INT(10) NOT NULL,
+    tag_id VARCHAR(15) NOT NULL,
     x VARCHAR(15),
     y VARCHAR(15),
     status INT(1) default 0,
