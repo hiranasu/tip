46c246f36860af34c1550b69de788bde4a7e9b42
gyoshida <boggy41@gmail.com>
2013-08-11 10:24:14 +0900

    カラム追加

--- tip/app/controllers/Tip.scala
+++ tip/app/controllers/Tip.scala
@@ -12,7 +12,7 @@ object Tip extends Controller {
         val id = request.queryString.get("tag").flatMap(_.headOption).mkString
         val params = id split '_' 
         val status = request.queryString.get("status").flatMap(_.headOption).mkString
-        var room: Room  = Room(params(1), params(2), status) 
+        var room: Room  = Room(params(0), params(1), params(2), status) 
         val result = room.add
         Ok("end")
   }
