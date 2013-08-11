ae378e8e44d0b22c01a1c7285fd8c24d3bb92a19
mhirakaw <mhirakaw@yahoo-corp.jp>
2013-08-11 11:21:32 +0900

    select追加

--- tip/app/models/room.scala
+++ tip/app/models/room.scala
@@ -6,17 +6,41 @@ import play.api.Play.current
 import anorm.SqlParser._
  
 case class Room (
-    room_id: String,
-    tag_id: String,
-    status: String) {
+    room_id:  String,
+    chair_id: String,
+    tag_id:   String,
+    x:        String,
+    y:        String,
+    status:   String) {
  
     def add {
         DB.withConnection { implicit c =>
-            val id: Int = SQL("update room_info set status={status} where room_id={room_id} and tag_id={tag_id}").
-                on('room_id-> this.room_id,
-                   'tag_id -> this.tag_id,
-                   'status -> this.status
+            val id: Int = SQL("update room_info set status={status} where room_id={room_id} and chair_id={chair_id} and tag_id={tag_id}").
+                on('room_id  -> this.room_id,
+                   'chair_id -> this.chair_id,
+                   'tag_id   -> this.tag_id,
+                   'status   -> this.status
                    ).executeUpdate()
         }
     }
+}
+
+object Room {
+  val data = {
+    get[String]("room_id") ~
+    get[String]("chair_id") ~
+    get[String]("tag_id") ~
+    get[String]("x") ~
+    get[String]("y") ~
+    get[String]("status") map {
+      case room_id ~ chair_id ~ tag_id ~ x ~ y ~ status => Room(room_id, chair_id, tag_id, x, y, status)
+    }
+  }
+
+  def getAll: List[Room] = {
+    DB.withConnection { implicit c =>
+      val datas = SQL("SELECT * FROM ROOM_INFO").as(Room.data *)
+      return datas
+    }
+  }
 }
